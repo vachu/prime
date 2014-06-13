e6e8081686f98bc91900b058180218b2cf7eeabc
Srivathsan Madhavan <vachu@github.fake>
2014-06-13 17:17:55 +0530

    fixed to work with cygwin64 bash-shell

--- gopath.bash
+++ gopath.bash
@@ -2,6 +2,8 @@
 path="`pwd`/mypkg"
 cnt=`echo $GOPATH | grep -c $path`
 if [ $cnt -eq 0 ] ; then
+	export GOPATH="`pwd`/mypkg"
+else
 	export GOPATH="$GOPATH:`pwd`/mypkg"
 fi
 echo "(new) GOPATH = $GOPATH"
